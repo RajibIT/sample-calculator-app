87f73db2fd1ebe4ed91e5e53fffd6c698370d73c
Rajibuddin Ahmed <41075901+RajibIT@users.noreply.github.com>
2021-07-17 01:14:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function c(val){
+	
+	document.getElementById("d").value = val;
+}
+
+function v(calculator)
+{
+	var val = calculator.getAttribute('data-value');
+	
+	document.getElementById("d").value+=val;
+}
+function op(calculator){
+	var operation = calculator.getAttribute('data-op');
+	document.getElementById("d").value += operation;
+}
+function e() 
+{ 
+try 
+{ 
+  c(eval(document.getElementById("d").value)) 
+} 
+catch(e) 
+{
+  c('Error') 
+} 
+}
